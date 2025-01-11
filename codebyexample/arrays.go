4cc108ff2f457666e6d5bf886f974fb72e6ec9de
satyam <satyamkale271@gmail.com>
2025-01-11 22:40:54 +0530

    2d array

--- codebyexample/arrays.go
+++ codebyexample/arrays.go
@@ -12,4 +12,16 @@ fmt.Println("len", len(a))
 b := [5]int{1,2,3,4,5}
 fmt.Print("dcl", b)
 
+b = [...]int{100,3 : 400,500} // redeclares array with same length  The elements at indices 1 and 2 are not explicitly set, so they default to 0.
+fmt.Println(b)
+
+
+var twod [2][3]int
+
+twod =[2][3]int {
+{1,2,3},
+{4,5,6,},
+}
+fmt.Println(twod)
+
 }
