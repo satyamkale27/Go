7b10650aaef04856c1c93adf624bd43332d075b2
satyam <satyamkale271@gmail.com>
2025-01-11 22:14:39 +0530

    array in go

--- codebyexample/arrays.go
+++ codebyexample/arrays.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func arrays(){
-var a [5]int
-fmt.Println("emp", a);
+ var a [5]int
+a[4]=100;
+fmt.Println("set", a);
+fmt.Println("get:", a[4])
+fmt.Println("len", len(a))
+
+b := [5]int{1,2,3,4,5}
+fmt.Print("dcl", b)
+
 }
